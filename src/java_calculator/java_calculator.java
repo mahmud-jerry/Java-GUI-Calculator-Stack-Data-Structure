c00db0c0530c06a50177275ba2baf917e636e437
mahmud.jerry <mahmud.jerry.me@gmail.com>
2020-09-22 13:32:37 +0600

    Initial Release
    This is my 4th semester java GUI Project . This is a simple  calculator but using infix evalution in stack data structure .

---
+++ src/java_calculator/java_calculator.java
@@ -0,0 +1,16 @@
+
+package java_calculator;
+
+import javax.swing.JOptionPane;
+
+
+public class java_calculator {
+
+
+    public static void main(String[] args) {
+
+      CalculatorBody calculatorBody = new CalculatorBody();
+      calculatorBody.setVisible(true);
+    }
+    
+}
